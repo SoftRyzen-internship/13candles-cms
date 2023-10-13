406ac85f6f3aad27acae87d12bd92eadb30fc569
Svitlana Zhovanik <lanasvetcat@gmail.com>
2023-10-13 17:17:41 +0300

    add starter kit Strapi and two plugins

---
+++ config/plugins.js
@@ -0,0 +1,29 @@
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+    },
+    graphql: {
+    config: {
+      endpoint: '/graphql',
+      shadowCRUD: true,
+      playgroundAlways: false,
+      depthLimit: 7,
+      amountLimit: 100,
+      apolloServer: {
+        tracing: false,
+      },
+    },
+  },
+});
